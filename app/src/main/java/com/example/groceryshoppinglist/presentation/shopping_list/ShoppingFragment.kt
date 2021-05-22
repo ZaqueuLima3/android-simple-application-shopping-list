1dff6bd4bcd7b09afee6c18fd40ad16712639729
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-22 18:11:27 -0300

    feature: create shopping list layout

---
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/shopping_list/ShoppingFragment.kt
@@ -0,0 +1,12 @@
+package com.example.groceryshoppinglist.presentation.shopping_list
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.groceryshoppinglist.R
+
+class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
