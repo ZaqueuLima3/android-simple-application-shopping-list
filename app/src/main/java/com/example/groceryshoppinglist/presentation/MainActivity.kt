d4fa762c4abef4495efb62e2bad9b79b200b5646
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-22 21:26:30 -0300

    chore: setup dependencies

--- app/src/main/java/com/example/groceryshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.groceryshoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.groceryshoppinglist.R
+import com.example.groceryshoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/groceryshoppinglist/presentation/shoppinglist/ShoppingFragment.kt
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/shoppinglist/ShoppingFragment.kt
@@ -1,12 +1,21 @@
 package com.example.groceryshoppinglist.presentation.shoppinglist
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.groceryshoppinglist.R
+import com.example.groceryshoppinglist.databinding.FragmentShoppingBinding
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+    private lateinit var binding: FragmentShoppingBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            requireActivity(),
+            R.layout.fragment_shopping
+        ) as FragmentShoppingBinding
     }
 }
