d33970d3818f7070ecd8c42e99f8d906d6acd866
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-22 22:35:55 -0300

    refactor: change Hilt annotaion Instalin from SingletonComponent to ApplicationComponent

--- app/src/androidTest/java/com/example/groceryshoppinglist/di/TestAppModule.kt
+++ app/src/androidTest/java/com/example/groceryshoppinglist/di/TestAppModule.kt
@@ -7,12 +7,12 @@ import com.example.groceryshoppinglist.shared.Constants.NAMED_MODULE
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Named
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ApplicationComponent::class)
 object TestAppModule {
     @Provides
     @Named(NAMED_MODULE)
