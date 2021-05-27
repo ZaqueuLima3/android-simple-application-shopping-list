25b0bc67d374da2c7b59f55f3ec241c831c4692a
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-27 17:52:13 -0300

    test: create FakeShoppingItemRepository

---
+++ app/src/test/java/com/example/groceryshoppinglist/data/repository/FakeShoppingItemRepository.kt
@@ -0,0 +1,38 @@
+package com.example.groceryshoppinglist.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.groceryshoppinglist.data.local.model.ShoppingItem
+
+class FakeShoppingItemRepository: ShoppingItemRepository {
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+    private val observableShoppingItem = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItem(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItem
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItem.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+}
