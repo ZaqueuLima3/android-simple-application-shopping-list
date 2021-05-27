4beb057d519e8608ffb47134af5488b442fd6229
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-27 18:35:20 -0300

    feature: create AddGroceryItemFragment

---
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/addgroceryitem/AddGroceryItemFragment.kt
@@ -0,0 +1,12 @@
+package com.example.groceryshoppinglist.presentation.addgroceryitem
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.groceryshoppinglist.R
+
+class AddGroceryItemFragment: Fragment(R.layout.fragment_add_grocery_item) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
