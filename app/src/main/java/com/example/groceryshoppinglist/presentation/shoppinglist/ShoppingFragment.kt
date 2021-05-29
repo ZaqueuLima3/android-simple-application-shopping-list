e33c7731a8943ef3229be790274d1c053ed33a82
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-29 12:18:10 -0300

    feature: ensure when click add new item btn navigate to add grocery fragment

--- app/src/main/java/com/example/groceryshoppinglist/presentation/shoppinglist/ShoppingFragment.kt
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/shoppinglist/ShoppingFragment.kt
@@ -4,20 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.groceryshoppinglist.R
 import com.example.groceryshoppinglist.databinding.FragmentShoppingBinding
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
-    private lateinit var binding: FragmentShoppingBinding
+    private var _binding: FragmentShoppingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentShoppingBinding.inflate(layoutInflater)
-        binding.btnAddNewItem.setOnClickListener { navigateToAddGroceryItem() }
+        _binding = FragmentShoppingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.shoppingListNewItemBtn.setOnClickListener { navigateToAddGroceryItem() }
+    }
+
+    private fun navigateToAddGroceryItem() {
+        findNavController().navigate(
+            ShoppingFragmentDirections.actionShoppingFragmentToAddGroceryItemFragment()
+        )
     }
 }
