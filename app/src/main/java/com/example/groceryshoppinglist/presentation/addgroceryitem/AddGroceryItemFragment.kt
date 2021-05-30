7421595b468ad8f506219aaba43be271ca64fb10
Zaqueu Santos <zaqueulima1@gmail.com>
2021-05-30 13:29:06 -0300

    feaure: ensure that pop to back stack when pressed back button

--- app/src/main/java/com/example/groceryshoppinglist/presentation/addgroceryitem/AddGroceryItemFragment.kt
+++ app/src/main/java/com/example/groceryshoppinglist/presentation/addgroceryitem/AddGroceryItemFragment.kt
@@ -4,19 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.groceryshoppinglist.R
 import com.example.groceryshoppinglist.databinding.FragmentAddGroceryItemBinding
+import com.example.groceryshoppinglist.shared.Status
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class AddGroceryItemFragment: Fragment(R.layout.fragment_add_grocery_item){
-    private lateinit var binding: FragmentAddGroceryItemBinding
+@AndroidEntryPoint
+class AddGroceryItemFragment : Fragment(R.layout.fragment_add_grocery_item) {
+    private var _binding: FragmentAddGroceryItemBinding? = null
+    private val binding get() = _binding!!
+    lateinit var addGroceryItemViewModel: AddGroceryItemViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentAddGroceryItemBinding.inflate(layoutInflater)
+        _binding = FragmentAddGroceryItemBinding.inflate(layoutInflater)
         return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addGroceryItemViewModel =
+            ViewModelProvider(requireActivity()).get(AddGroceryItemViewModel::class.java)
+        setOnBackPressed()
+    }
+
+    private fun setOnBackPressed() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
 }
